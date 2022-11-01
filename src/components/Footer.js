7aa01f94b9745c1111bff885da9af77cec6f6c42
sione-tenifa <johntenifa@yahoo.com>
2022-11-01 16:53:44 -0600

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,21 @@
 import React from "react";
-import InstagramIcon from "@material-ui/icons/Instagram";
-import TwitterIcon from "@material-ui/icons/Twitter";
-import FacebookIcon from "@material-ui/icons/Facebook";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
+import GithubIcon from "@material-ui/icons/GitHub";
 import "../styles/Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <InstagramIcon />
-        <TwitterIcon />
-        <FacebookIcon />
-        <LinkedInIcon />
+      <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/sionetenifa/'>
+            <LinkedInIcon  />
+            </a>
+            <a href="/contact" rel="noreferrer"> 
+            <EmailIcon /></a>
+            <a target='_blank' rel='noreferrer' href='https://github.com/Sione-Tenifa'>
+              <GithubIcon />
+            </a>
       </div>
       <p> &copy; 2022 Sione Tenifa</p>
     </div>
